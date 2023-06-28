79b684df61c0759196a1c4007c713cd34d22a417
KeitaroHamada <hamataro1029@gmail.com>
2023-06-28 15:49:05 +0900

    totyuu

--- app.js
+++ app.js
@@ -1,47 +1,70 @@
-const question = "僕のすきな食べ物は何?";
-const answers = [
-    "からあげ",
-    "とんかつ",
-    "和風オムライス",
-    "うどん"
-];
-
-const correct = "和風オムライス";
-console.log();
-document.getElementById("js-question").textContent = question;
+const quiz=[
+    {
+        question: "僕のすきな食べ物は何?",
+        answers: [
+            "からあげ",
+            "とんかつ",
+            "和風オムライス",
+            "うどん"
+        ],
+        correct: "和風オムライス"
+    },{
+        question: "僕の出身小学校は？",
+        answers: [
+            "厚生小学校",
+            "宮山小学校",
+            "明倫小学校",
+            "有緝小学校"
+        ],
+        correct: "厚生小学校"
+    },{
+        question: "僕がやったことのない習い事は？",
+        answers: [
+            "そろばん",
+            "サッカー",
+            "水泳",
+            "茶道"
+        ],
+        correct: "茶道"
+    }
+]
+const quizLength = quiz.length;
+let quizIndex = 0;
+while(quizIndex < quizLength){
+    quizIndex++;
+}
 
 const $button = document.getElementsByTagName("button");
+let buttonLength = $button.length;
 
-$button[0].textContent=answers[0];
-$button[1].textContent=answers[1];
-$button[2].textContent=answers[2];
-$button[3].textContent=answers[3];
+const setupQuiz = () => {
+    document.getElementById("js-question").textContent = quiz[quizIndex].question;
+    let buttonIndex = 0;
+    while(buttonIndex < buttonLength){
+        $button[buttonIndex].textContent=quiz[quizIndex].answers[buttonIndex];
+        buttonIndex++;
+    }
+}
+setupQuiz();
 
-$button[0].addEventListener("click", () => {
-    if(correct === $button[0].textContent){
+const clickHandler = (e) => {
+    if(quiz[quizIndex].correct === e.target.textContent){
         window.alert("正解!");
     }else{
         window.alert("不正解!");
     }
-});
-$button[1].addEventListener("click", () => {
-    if(correct === $button[1].textContent){
-        window.alert("正解!");
+    quizIndex++;
+    if(quizIndex < quizLength){
+        setupQuiz();
     }else{
-        window.alert("不正解!");
+        window.alert("終わり!");
     }
-});
-$button[2].addEventListener("click", () => {
-    if(correct === $button[2].textContent){
-        window.alert("正解!");
-    }else{
-        window.alert("不正解!");
-    }
-});
-$button[3].addEventListener("click", () => {
-    if(correct === $button[3].textContent){
-        window.alert("正解!");
-    }else{
-        window.alert("不正解!");
-    }
-});
+}
+let handlerIndex=0;
+while(handlerIndex < buttonLength){
+    $button[handlerIndex].addEventListener("click", (e) => {
+        clickHandler(e);
+    });
+    handlerIndex++;
+}
+
